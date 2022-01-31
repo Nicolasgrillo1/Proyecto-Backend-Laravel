4ef30c76e8547d6c5acf03985aeac88221f301fd
unknown <nicolasjgrillo@gmail.com>
2022-01-31 12:13:30 -0300

    error cors existent

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,13 +14,15 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        // \App\Http\Middleware\TrustHosts::class,
+        \App\Http\Middleware\TrustProxies::class,
+        \Fruitcake\Cors\HandleCors::class,
+        \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\Cors::class,//cors added here 
-         ];
+    ];
 
     /**
      * The application's route middleware groups.
