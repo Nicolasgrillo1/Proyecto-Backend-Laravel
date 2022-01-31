9cccc9f513ab160b0f88d4c78556256114abeca7
unknown <nicolasjgrillo@gmail.com>
2022-01-31 10:56:37 -0300

    docker

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/home', function () {
-    return view('25watts');
+    return view('hola');
 });
 
 // Route::post('/saveUsers',"App\Http\Controllers\userController@saveUser")->name("saveUsers");
