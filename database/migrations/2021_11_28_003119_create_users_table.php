7056fe569348325e0959ee34605c32fe5687d23e
unknown <nicolasjgrillo@gmail.com>
2022-01-31 20:02:45 -0300

    modified migration add

--- database/migrations/2021_11_28_003119_create_users_table.php
+++ database/migrations/2021_11_28_003119_create_users_table.php
@@ -20,8 +20,8 @@ class CreateUsersTable extends Migration
             // $table->timestamp('email_verified_at')->nulleable();
             $table->string ('phone');
             $table->text ('message');
-            // $table->rememberToken();
-            // $table->timestamps();
+            $table->rememberToken();
+            $table->timestamps();
         });
     }
 
