ae6d85db0b1970123c03f5fc529239f656e89fb8
unknown <nicolasjgrillo@gmail.com>
2022-02-01 00:01:15 -0300

    Modificaciones en la ruta

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/home', function () {
-    return view('hola');
+Route::get('/', function () {
+    return view('Welcome');
 });
+
 
 // Route::post('/saveUsers',"App\Http\Controllers\userController@saveUser")->name("saveUsers");
